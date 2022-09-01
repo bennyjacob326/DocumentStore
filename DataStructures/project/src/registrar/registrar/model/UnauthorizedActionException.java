384360b342b9b1cf7b35ea32659be698f8964512
bennyjacob326 <bennyjacob326@gmail.com>
2022-08-31 20:58:53 -0400

    reorganized files

--- DataStructures/project/src/registrar/registrar/model/UnauthorizedActionException.java
+++
@@ -1,7 +0,0 @@
-package se_practice.week2.registrar.registrar.model;
-
-/**
- * Thrown when a user attempts to read or write data when he does not have permission to do so
- */
-public class UnauthorizedActionException extends RuntimeException{
-}
--- DataStructures/project/stage3/src/main/java/edu/yu/cs/com1320/project/Undoable.java
+++
@@ -1,8 +0,0 @@
-package edu.yu.cs.com1320.project;
-
-public interface Undoable {
-    /**
-     * @return true if the undo succeeds
-     */
-    boolean undo();
-}
